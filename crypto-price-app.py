6c5791bea5007cc0847ab1a5a2642b9a50f1570f
thedon122 <donovan122@yahoo.com>
2021-02-11 23:43:58 -0500

    About me and page layout

--- crypto-price-app.py
+++ crypto-price-app.py
@@ -21,3 +21,27 @@ st.title('Crypto Price App')
 st.markdown("""
 This app retrieves cryptocurrency prices for the top 100 cryptocurrency from the **CoinMarketCap**!
 """)
+
+# About
+expander_bar = st.beta_expander("About")
+expander_bar.markdown("""
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn, BeautifulSoup, requests, json, time
+* **Data source:** [CoinMarketCap](http://coinmarketcap.com).
+* **Credit:** Web scraper adapted from the Medium article *[Web Scraping Crypto Prices With Python](https://towardsdatascience.com/web-scraping-crypto-prices-with-python-41072ea5b5bf)* written by [Bryan Feng](https://medium.com/@bryanf).
+""")
+
+
+#---------------------------------#
+# Page layout (continued)
+## Divide page to 3 columns (col1 = sidebar, col2 and col3 = page contents)
+col1 = st.sidebar
+col2, col3 = st.beta_columns((2,1))
+
+#---------------------------------#
+# Sidebar + Main panel
+col1.header('Input Options')
+
+## Sidebar - Currency price unit
+currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
+
+# Web scraping of CoinMarketCap data
